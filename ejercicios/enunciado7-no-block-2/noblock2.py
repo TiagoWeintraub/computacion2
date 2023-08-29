a416c4ca8ecec3b77f6a28615d3ac51fbf0c0e8a
tiago <tiagoweintraub23@gmail.com>
2023-08-29 16:02:36 -0300

    Ej 7 terminado

--- ejercicios/enunciado7-no-block-2/noblock2.py
+++ ejercicios/enunciado7-no-block-2/noblock2.py
@@ -3,6 +3,47 @@
 
 # Realizar todo utilizando multiprocessing
 
+import multiprocessing
+import time
+import hashlib
+
+def noblock(diff):
+    nonce = 0
+    while not is_valid_hash(nonce, diff):
+        nonce += 1
+        return nonce
+
+def is_valid_hash(nonce, diff):
+    
+    hash = hashlib.sha256(str(nonce).encode()).hexdigest()
+    return hash.startswith("0" * diff)
+
+def main():
+
+    diff = 5
+    procesos = []
+    for i in range(10):
+        proceso = multiprocessing.Process(target=noblock, args=(diff,))
+        procesos.append(proceso)
+        proceso.start()
+
+    found = False
+    nonce = None
+    for proceso in procesos:
+        proceso.join()
+        if not found:
+            if proceso.is_alive():
+                continue
+            found = True
+            nonce = proceso.exitcode
+
+    if found:
+        print(f"El nonce encontrado es: {nonce}")
+    else:
+        print("No se ha encontrado el nonce")
+
+if __name__ == "__main__":
+    main()
 
 
 
